180f9f0217ed88c7571fec6d554dadc3542970d7
v_jjinglei <v_jjinglei@tencent.com>
2020-08-18 14:46:35 +0800

    router

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import Element from 'element-ui'
+import router from './router'
 
 Vue.config.productionTip = false
 
@@ -10,5 +11,7 @@ Vue.prototype.$bus = new Vue()
 Vue.use(Element)
 
 new Vue({
+  // 挂载，目的是什么？让我们可以在插件中访问到Router实例
+  router,
   render: h => h(App),
 }).$mount('#app')
