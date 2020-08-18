5fcceb9d7a9e5e42271c647b2efe5dc91d7f182c
v_jjinglei <v_jjinglei@tencent.com>
2020-08-18 16:22:49 +0800

    vue add vuex

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import Element from 'element-ui'
+
 // import router from './router'
 import router from './krouter'
+
+import store from './store'
 
 Vue.config.productionTip = false
 
@@ -14,5 +17,6 @@ Vue.use(Element)
 new Vue({
   // 挂载，目的是什么？让我们可以在插件中访问到Router实例
   router,
-  render: h => h(App),
+  store,
+  render: h => h(App)
 }).$mount('#app')
---
+++ src/store/index.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  //状态、数据 
+  state: {
+  },
+  //更改状态的函数
+  mutations: {
+  },
+  //异步操作 
+  actions: {
+  },
+  //包含以上概念的容器
+  modules: {
+  }
+})
